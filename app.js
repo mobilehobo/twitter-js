5d6f35c3365e88ed6fc76a2b8a71c30e9fe409af
David Geiss <dave.geiss@gmail.com>
2017-06-12 18:33:59 -0400

    basic logging

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.listen(3000);
 
+app.use('/', (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
+
 app.get('/', (req, res) => {
+    //res.status(200);
     res.send('Bienvenidos!');
 });
